d1539bbc96e456526c6d501261f04e2bd6172d24
Basiczombie <issacgable@gmail.com>
2016-05-24 10:48:11 -0600

    Refactored plugin to new standard

--- src/scripts/sm-links/js/smlinks.js
+++ src/scripts/sm-links/js/smlinks.js
@@ -2,9 +2,12 @@
 (function ($){
   $(document).ready(function (){
     var nav_functions = $('#left_nav_functions');
-    nav_functions.append('/scripts/sm-links/html/sm_links.html');
+    fetch('/scripts/sm-links/html/sm_links.html')
+      .then(response => response.text())
+      .then(body => nav_functions.append(body))
 
-    if(studentFunctions){
+
+    if(typeof studentFunctions !== 'undefined'){
       studentFunctions.forEach((object, index) => {
         if(object.name === "Functions"){
           studentFunctions.splice(index+5,0, {
@@ -23,11 +26,11 @@
 
     if($('h1:contains(Special Functions)').length){
       var table = $('table tbody');
-      var insertSelector = table.find('tr td:contains(Interfaces to other systems)');
-      fetch('/scripts/sm-links/html/sm_links.html')
+      var insertSelector = table.find('tr:contains(Interfaces to other systems)');
+      fetch('/scripts/sm-links/html/sm_table_fragment.html')
         .then(response => response.text())
         .then(body => {
-          body.insertAfter(insertSelector);
+          insertSelector.after(body);
           //Fix row colors
           $('tr.oddRow').removeClass('oddRow');
           $('tr:odd').addClass('oddRow');
