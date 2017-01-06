51ce42cc9bbaa5ad56096cd016fa6942cda72356
jonesnc <purkinje90@gmail.com>
2017-01-06 09:26:18 -0700

    Use es2015 module syntax, code reformatting

--- src/scripts/sm-links/js/smlinks.js
+++ src/scripts/sm-links/js/smlinks.js
@@ -1,30 +1,30 @@
-/* global $j*/
-(function ($){
-  $(document).ready(function (){
+import $ from 'jquery';
+
+export default function() {
+  $(() => {
     var nav_functions = $('#left_nav_functions');
     fetch('/scripts/sm-links/html/sm_links.html')
       .then(response => response.text())
-      .then(body => nav_functions.append(body))
+      .then(body => nav_functions.append(body));
 
-
-    if(typeof studentFunctions !== 'undefined'){
+    if (typeof studentFunctions !== 'undefined') {
       studentFunctions.forEach((object, index) => {
-        if(object.name === "Functions"){
-          studentFunctions.splice(index+5,0, {
+        if (object.name === "Functions") {
+          studentFunctions.splice(index + 5, 0, {
             address: "/admin/messagesender/msgsndr.html",
             name: "Parent Notification"
           });
         }
-      })
+      });
     }
 
-    if($('select[name="page"]').length){
+    if ($('select[name="page"]').length) {
       var option = $('<option value="/admin/messagesender/msgsndr.html">Parent Notification</option>');
       var index = 19;
       option.insertBefore('select[name="page"] option:nth-child(' + index + ')');
     }
 
-    if($('h1:contains(Special Functions)').length){
+    if ($('h1:contains(Special Functions)').length) {
       var table = $('table tbody');
       var insertSelector = table.find('tr:contains(Interfaces to other systems)');
       fetch('/scripts/sm-links/html/sm_table_fragment.html')
@@ -34,9 +34,7 @@
           //Fix row colors
           $('tr.oddRow').removeClass('oddRow');
           $('tr:odd').addClass('oddRow');
-        })
-
-
+        });
     }
   });
-})($j);
+}
